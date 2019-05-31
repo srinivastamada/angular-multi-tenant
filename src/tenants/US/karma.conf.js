2bdee37dd89e270562a80077fcac8610d656d775
Srinivas Tamada <srinivas@9lessons.info>
2019-05-31 01:02:08 -0400

    architecture

--- src/tenants/US/karma.conf.js
+++ src/tenants/US/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/angular-multi-tenant'),
+      dir: require('path').join(__dirname, '../coverage/US'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
