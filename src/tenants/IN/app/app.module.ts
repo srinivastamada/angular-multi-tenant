62b4c901950942da51c3647329dad990161cfc80
Srinivas Tamada <srinivas@9lessons.info>
2019-06-10 15:01:42 +0000

    IN Package

---
+++ src/tenants/IN/app/app.module.ts
@@ -0,0 +1,24 @@
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+  NO_ERRORS_SCHEMA
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedComponentsModule } from '@shared/shared-components/shared-components.module';
+import { ComponentsModule } from '../components/components.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule,
+    SharedComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {}
