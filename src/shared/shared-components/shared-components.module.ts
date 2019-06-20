38c703d64b6a07650234aa470836250de2b4d3f1
Srinivas Tamada <srinivas@9lessons.info>
2019-06-20 01:22:44 -0400

    colors

--- src/shared/shared-components/shared-components.module.ts
+++ src/shared/shared-components/shared-components.module.ts
@@ -2,10 +2,11 @@ import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LogoComponent } from './logo/logo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [LogoComponent, FooterComponent],
-  exports: [LogoComponent, FooterComponent],
+  declarations: [LogoComponent, FooterComponent, HeaderComponent],
+  exports: [LogoComponent, FooterComponent, HeaderComponent],
   imports: [CommonModule]
 })
 export class SharedComponentsModule {}
